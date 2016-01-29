eb0970e8ebb2bb0470c8008f1abe256a285e6924
iCodr8 <icodr8@gmail.com>
2016-01-30 00:35:41 +0100

    init

---
+++ src/Craffft/TranslationFieldsBundle/CraffftCurrenciesBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Translation Fields Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Craffft\TranslationFieldsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Craffft Translation Fields Bundle.
+ */
+class CraffftTranslationFieldsBundle extends Bundle
+{
+}
