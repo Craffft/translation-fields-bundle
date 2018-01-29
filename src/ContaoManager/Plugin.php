aaacc3391d4860cf4ea2d6a83b588e001d0534b3
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:06:47 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Translation Fields Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Craffft\TranslationFieldsBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
@@ -8,7 +19,7 @@ use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Craffft\TranslationFieldsBundle\CraffftTranslationFieldsBundle;
 
-class Plugin
+class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
